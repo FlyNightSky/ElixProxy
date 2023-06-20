ce43c2223f40d873071d02f7afafa70bfc3d50a6
StreamScribe <118761018+FlyNightSky@users.noreply.github.com>
2023-06-20 11:43:58 -0400

    mistake

--- public/client.js
+++ public/client.js
@@ -1,26 +1,32 @@
-const proxyForm = document.getElementById('proxy-form');
-const urlInput = document.getElementById('url-input');
-const resultContainer = document.getElementById('result-container');
-
-proxyForm.addEventListener('submit', async (event) => {
+const proxyForm = document.getElementById("proxy-form");
+const title = document.getElementById("title");
+const urlInput = document.getElementById("url-input");
+const resultContainer = document.getElementById("result-container");
+const container = document.getElementsByClassName("container");
+resultContainer.style.display = "none";
+urlInput.value = "https://";
+proxyForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const url = urlInput.value;
 
   try {
-    const response = await fetch(`/Elix?url=${encodeURIComponent(url)}`); // Updated endpoint
+    const response = await fetch(`/Elix?url=${encodeURIComponent(url)}`);
 
     if (response.ok) {
       const data = await response.text();
       resultContainer.innerHTML = data;
-      urlInput.style.display = 'none';
-      proxyForm.style.display = 'none';
+      title.style.display = "none";
+      urlInput.style.display = "none";
+      proxyForm.style.display = "none";
+      resultContainer.style.display = "block";
+      document.body.style.backgroundColor = "white";
     } else {
-      resultContainer.innerHTML = 'Error retrieving the URL.';
+      resultContainer.innerHTML = "Error retrieving the URL.";
     }
   } catch (error) {
-    resultContainer.innerHTML = 'Error occurred while making the request.';
+    resultContainer.innerHTML = "Error occurred while making the request.";
   }
 });
 
 // Show the proxy form and URL input initially
-
+container[0].style.display = "block";
--- server.js
+++ server.js
@@ -1,55 +1,55 @@
-const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
-const path = require('path');
+const express = require("express");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const path = require("path");
 
 const app = express();
 const port = 8080;
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Web proxy endpoint
-app.get('/Elix', async (req, res) => {
+app.get("/Elix", async (req, res) => {
   try {
     const url = req.query.url;
     const response = await axios.get(url);
 
     // Extract the content type from the response headers
-    const contentType = response.headers['content-type'];
+    const contentType = response.headers["content-type"];
 
     // Check if the response is HTML
-    if (contentType.includes('text/html')) {
+    if (contentType.includes("text/html")) {
       const html = response.data;
       const $ = cheerio.load(html);
 
       // Rewrite HTML links
-      $('a[href]').each((_, element) => {
+      $("a[href]").each((_, element) => {
         const link = $(element);
-        const href = link.attr('href');
-        if (href && !href.startsWith('#')) {
+        const href = link.attr("href");
+        if (href && !href.startsWith("#")) {
           const absoluteUrl = new URL(href, url).href;
-          link.attr('href', `/Elix?url=${encodeURIComponent(absoluteUrl)}`);
+          link.attr("href", `/Elix?url=${encodeURIComponent(absoluteUrl)}`);
         }
       });
 
       // Rewrite <img> tags
-      $('img[src]').each((_, element) => {
+      $("img[src]").each((_, element) => {
         const img = $(element);
-        const src = img.attr('src');
+        const src = img.attr("src");
         if (src) {
           const absoluteUrl = new URL(src, url).href;
-          img.attr('src', `/ElixImg?url=${encodeURIComponent(absoluteUrl)}`);
+          img.attr("src", `/ElixImg?url=${encodeURIComponent(absoluteUrl)}`);
         }
       });
 
       // Rewrite <link> tags for CSS files
       $('link[rel="stylesheet"]').each((_, element) => {
         const link = $(element);
-        const href = link.attr('href');
+        const href = link.attr("href");
         if (href) {
           const absoluteUrl = new URL(href, url).href;
-          link.attr('href', `/ElixCss?url=${encodeURIComponent(absoluteUrl)}`);
+          link.attr("href", `/ElixCss?url=${encodeURIComponent(absoluteUrl)}`);
         }
       });
 
@@ -61,36 +61,38 @@ app.get('/Elix', async (req, res) => {
       res.send(response.data);
     }
   } catch (error) {
-    console.error('Error retrieving the URL:', error);
-    res.status(500).send('Error retrieving the URL.');
+    console.error("Error retrieving the URL:", error);
+    res.status(500).send("Error retrieving the URL.");
   }
 });
 
 // Image proxy endpoint
-app.get('/ElixImg', async (req, res) => {
+app.get("/ElixImg", async (req, res) => {
   try {
     const imageUrl = req.query.url;
-    const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const imageResponse = await axios.get(imageUrl, {
+      responseType: "arraybuffer",
+    });
 
-    res.set('Content-Type', imageResponse.headers['content-type']);
+    res.set("Content-Type", imageResponse.headers["content-type"]);
     res.send(imageResponse.data);
   } catch (error) {
-    console.error('Error retrieving the image:', error);
-    res.status(500).send('Error retrieving the image.');
+    console.error("Error retrieving the image:", error);
+    res.status(500).send("Error retrieving the image.");
   }
 });
 
 // CSS proxy endpoint
-app.get('/ElixCss', async (req, res) => {
+app.get("/ElixCss", async (req, res) => {
   try {
     const cssUrl = req.query.url;
     const cssResponse = await axios.get(cssUrl);
 
-    res.set('Content-Type', 'text/css');
+    res.set("Content-Type", "text/css");
     res.send(cssResponse.data);
   } catch (error) {
-    console.error('Error retrieving the CSS:', error);
-    res.status(500).send('Error retrieving the CSS.');
+    console.error("Error retrieving the CSS:", error);
+    res.status(500).send("Error retrieving the CSS.");
   }
 });
 
